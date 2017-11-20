1941f9db467e3845ee93cc210844c2d7420dde43
Hossein Taghi-Zadeh <h.t.azeri@gmail.com>
2017-11-20 23:58:32 +0330

    first release

---
+++ PersianStemmer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stemming.Persian;
+
+namespace PersianStemmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ps = new Stemmer();
+            //Console.WriteLine(ps.run("زیباست"));
+            Console.WriteLine(ps.run("پدران"));
+
+            Console.ReadKey();
+        }
+    }
+}
